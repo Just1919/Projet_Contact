3dfeaab4b0f075fa1d9f033e6685f552ba175b55
Just1919 <justindjakas4@gmail.com>
2023-12-19 11:33:39 +0100

    Gestion de conflit

--- app/src/main/java/com/example/contactpro/Home.java
+++ app/src/main/java/com/example/contactpro/Home.java
@@ -1,5 +1,5 @@
 package com.example.contactpro;
-//Déclaration  et initialisation
+//Déclaration  et initialisation S
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
